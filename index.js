eda8f4f63b773313d8e42de739d9c447a5934d46
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-08-03 19:19:30 +0300

    task

---
+++ index.js
@@ -0,0 +1,13 @@
+/*let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+    },
+    down: function () {
+        this.step--;
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+    }
+}; */
+
